5087a4f2a2bf60371942f3c47d2fb2005df57d9f
Your Name <you@example.com>
2025-11-08 18:25:25 +0530

    Added default route to fix Cannot GET /

--- Backend/server.js
+++ Backend/server.js
@@ -30,6 +30,10 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 //Server uploads folder
 app.use("/uploads",express.static(path.join(__dirname, "uploads")));
+app.get("/", (req, res) => {
+  res.send("✅ Expense Tracker Backend is running successfully!");
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
